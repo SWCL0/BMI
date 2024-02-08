b401ec6f236e3e265c2fdeeee9df58556ca12da4
SWCL0 <stan.w.c@icloud.com>
2024-02-08 11:43:15 +0100

    update BMI.php
    Toevoegingen:
    Gewicht KG opvraag
    Lengte CM opvraag
    Berekening lengte cm > lengte m
    Berekening BMI

--- BMI.php
+++ BMI.php
@@ -1,5 +1,12 @@
 <?php
 
+echo "Wat is uw gewicht in KG: ";
+$gewicht = readline();
 
+echo "Wat is uw lengte in CM: ";
+$legnteCM = readline();
+
+$lengteM = $lengteCM / 100;
+$BMI =  $gewicht / ($lengteM * $lengteM)
 
 ?>
