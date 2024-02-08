c566a54206434719109cf8ff4f4218c9237a82d9
SWCL0 <stan.w.c@icloud.com>
2024-02-08 12:09:03 +0100

    Update BMI.php
    Toevoeging switch statements > geeft index weer

--- BMI.php
+++ BMI.php
@@ -4,9 +4,26 @@ echo "Wat is uw gewicht in KG: ";
 $gewicht = readline();
 
 echo "Wat is uw lengte in CM: ";
-$legnteCM = readline();
+$lengteCM = readline();
 
 $lengteM = $lengteCM / 100;
-$BMI =  $gewicht / ($lengteM * $lengteM)
+$BMI =  $gewicht / ($lengteM * $lengteM);
+
+echo "U heeft een BMI van $BMI" . PHP_EOL;
+
+switch ($BMI) {
+    case ($BMI < "18.5"):
+        echo "Uitkomst index: Onder gewicht";
+    break;
+    case ($BMI >= "18.5") && ($BMI <= "25"):
+        echo "Uitkomst index: Gezond gewicht";
+    break;
+    case ($BMI >= "25") && ($BMI <= "30"):
+        echo "Uitkomst index: Overgewicht";
+    break;
+    case ($BMI >= "30"):
+        echo "Uitkomst index: Ernstig overgewicht (obesitas)";
+    break;
+}
 
 ?>
